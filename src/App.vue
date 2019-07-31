d44498fb3db3e263a065e012808d14b9377ed53b
Angelina <angyqzz@gmail.com>
2019-08-01 02:24:29 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div class="card-title">Eat all the slices!</div>
+    <div class="card-text">Click on each slice to destroy it 😀</div>
+    <div class="plate">
+      <div class="cake">
+        <div
+          v-for="n in 6"
+          class="cake-slice"
+          :class="{'hidden' : hidden[n]}"
+          @click="setEaten(n)"
+          :key="n"
+        ></div>
+        <div class="cake-decoration">Happy <br /> Birthday</div>
+      </div>
+    </div>
+    <card />
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Card from "./Card";
+export default {
+  name: "app",
+  components: {
+    Card
+  },
+  methods: {
+    setEaten(n) {
+      Vue.set(this.hidden, n, true);
+    }
+  },
+  data() {
+    return {
+      hidden: {}
+    };
+  }
+};
+</script>
+
+<style lang="less">
+@import "style.less";
+</style>
---
+++ src/Card.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card-overlay">
+    <div class="card">
+      Hello
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Card",
+  props: [],
+  components: {},
+  computed: {},
+  methods: {},
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="less" scoped>
+.card-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  z-index: 9999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .card {
+    background: #fff;
+    color: rgba(33, 33, 33, 1);
+    width: 80%;
+    margin: 0 auto;
+    max-width: 400px;
+  }
+}
+</style>
